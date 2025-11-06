8a80206fce048595bbbdd9aa76a3af17446c1b8b
arpandowari <arpandoari@gmail.com>
2025-11-06 21:19:21 +0530

    save

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Admin Panel - ORION AI')</title>
+    <link rel="icon" type="image/png" href="{{ asset('storage/logo/orion-logo.png') }}">
+    <link rel="shortcut icon" type="image/png" href="{{ asset('storage/logo/orion-logo.png') }}">
     <style>
         * {
             margin: 0;
