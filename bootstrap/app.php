dd232e14435654ff9e3387b2fa79e7606298b5b5
arpandowari <arpandoari@gmail.com>
2025-11-10 12:48:41 +0530

    upadte the force request

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,6 +14,9 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'student.auth' => \App\Http\Middleware\StudentAuth::class,
         ]);
+        
+        // Force HTTPS in production
+        $middleware->append(\App\Http\Middleware\ForceHttps::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
